f990f0b8e5308f7343b0631fb6d861f083aa239f
Chris Lord <chrislord.net@gmail.com>
2017-07-27 18:29:53 +0100

    Rewrite labelling/infer.py to run in a reasonable timeframe

--- training/labelling/infer.py
+++ training/labelling/infer.py
@@ -6,61 +6,72 @@ import png
 import sys
 import pickle
 import numpy as np
+from collections import deque
 
 def get_depth(path):
     exr = cv2.imread(path, cv2.IMREAD_UNCHANGED)
     return exr[...,0]
 
-def eval_pixel(node, depth, x):
-    clip = np.array(np.shape(depth)) - 1
-    dx = depth[x[0]][x[1]]
+def eval_image(node, depth):
+    depth = np.array(depth)
+    shape = np.array(np.shape(depth))
+    clip = shape - 1
 
-    while 't' in node:
-        u = np.around(np.clip(np.add(x, np.divide(node['u'], dx)), [0, 0], clip)).astype(np.int32)
-        v = np.around(np.clip(np.add(x, np.divide(node['v'], dx)), [0, 0], clip)).astype(np.int32)
-        fx = depth[u[0]][u[1]] - depth[v[0]][v[1]]
-        if fx < node['t']:
-            node = node['l']
-        else:
-            node = node['r']
+    n_pixels = shape[0] * shape[1]
+    coords = deque(np.reshape(np.stack(np.indices(shape), axis=2), [n_pixels, 2]))
 
-    return node['label_probs']
+    output = [[None for x in range(shape[1])] for y in range(shape[0])]
+    nodes = deque([node for i in range(n_pixels)])
 
-def eval_image(tree, depth):
-    shape = np.shape(depth)
-    rows, cols = np.indices(shape)
-    coords = np.reshape(np.stack((rows, cols), axis=2), (shape[0] * shape[1], 2))
+    unrolled_depth = np.reshape(depth, [n_pixels])
+    pixels = deque(np.stack([unrolled_depth, unrolled_depth], axis=1))
 
-    output = {}
-    n_pixels = len(coords)
-    for x in coords:
-        output[tuple(x)] = eval_pixel(tree, depth, x)
-        n_pixels -= 1
-        if n_pixels % 100 == 0:
-            sys.stdout.write('%d pixels left %s     \r' % (n_pixels, str(tuple(x))))
-            sys.stdout.flush()
+    current_depth = 0
+    while len(nodes) > 0:
+        nodes_u = [node['u'] for node in nodes]
+        nodes_v = [node['v'] for node in nodes]
+
+        u = np.around(np.clip(np.add(coords, np.divide(nodes_u, pixels)), [0, 0], clip)).astype(np.int32)
+        v = np.around(np.clip(np.add(coords, np.divide(nodes_v, pixels)), [0, 0], clip)).astype(np.int32)
+
+        nodes = deque( \
+            [node['l'] if depth[u[0]][u[1]] - depth[v[0]][v[1]] < node['t'] else \
+             node['r'] for node, u, v in zip(nodes, u, v)])
+
+        # Check if we've hit any leaves
+        for i in range(len(nodes)):
+            node = nodes.popleft()
+            coord = coords.popleft()
+            pixel = pixels.popleft()
+
+            if 't' not in node:
+                output[coord[0]][coord[1]] = node['label_probs']
+                n_pixels -= 1
+            else:
+                nodes.append(node)
+                coords.append(coord)
+                pixels.append(pixel)
+
+        sys.stdout.write('(%d) %d pixels left       \r' % (current_depth, n_pixels))
+        sys.stdout.flush()
+        current_depth += 1
 
     return output
 
 def generate_png(label_probs, name):
+    image = [[0 for x in range(len(label_probs[0]))] for y in range(len(label_probs))]
+    for y in range(len(label_probs)):
+        for x in range(len(label_probs[y])):
 
-    image = [[]]
-    for coord, probs in label_probs.items():
-        row = coord[0]
-        col = coord[1]
-        while row >= len(image):
-            image.append([])
-        while col >= len(image[row]):
-            image[row].append(0)
+            probs = label_probs[y][x]
+            label = 0
+            prob = -1
+            for key, value in probs.items():
+                if value > prob:
+                    prob = value
+                    label = key
 
-        label = 0
-        prob = -1
-        for key, value in probs.items():
-            if value > prob:
-                prob = value
-                label = key
-
-        image[row][col] = int(label)
+            image[y][x] = int(label)
 
     with open(name, 'wb') as f:
         w = png.Writer(len(image[0]), len(image), greyscale=True)
