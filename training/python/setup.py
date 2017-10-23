9ae347544f060487628d8bac9de74931573c5344
Chris Lord <chrislord.net@gmail.com>
2017-10-23 16:27:34 +0100

    training: Make sure build_ext happens first for python bindings

--- training/python/setup.py
+++ training/python/setup.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
 
 from setuptools import setup, Extension
+from distutils.command.build import build
 
 import numpy
 
@@ -8,6 +9,12 @@ try:
     numpy_include = numpy.get_include()
 except AttributeError:
     numpy_include = numpy.get_numpy_include()
+
+class BuildExtFirst(build):
+    sub_commands = [('build_ext', build.has_ext_modules),
+                    ('build_py', build.has_pure_modules),
+                    ('build_clib', build.has_c_libraries),
+                    ('build_scripts', build.has_scripts)]
 
 glimpse = Extension('_glimpse',
         ['glimpse.i', 'glimpse_python.cc'],
@@ -21,6 +28,7 @@ setup(name = 'glimpse',
       version = '0.0.1',
       package_dir = { 'glimpse': '.' },
       packages = [ 'glimpse' ],
+      cmdclass = { 'build': BuildExtFirst },
       ext_modules = [ glimpse ],
       license = 'Proprietary',
       url = 'https://medium.com/impossible/glimpse-a-sneak-peak-into-your-creative-self-29bd2e656ff6',
