adf309f82e4aea2cab99fd90fa4bad61baf512cd
Robert Bragg <robert@impossible.com>
2017-11-28 18:08:23 +0000

    Add MIT license copyright headers

--- src/pack.h
+++ src/pack.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017 Kwamecorp
+ * Copyright (C) 2017 Glimp IP Ltd
  *
  * Permission is hereby granted, free of charge, to any person
  * obtaining a copy of this software and associated documentation
