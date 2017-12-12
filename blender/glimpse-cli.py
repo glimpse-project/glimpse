4485dd61a79f216a7b1b23e2e24df7469607ad01
Robert Bragg <robert@impossible.com>
2017-12-12 21:20:47 +0000

    blender/glimpse-cli: detect when required addon not enabled

--- blender/glimpse-cli.py
+++ blender/glimpse-cli.py
@@ -1,10 +1,31 @@
+# Copyright (c) 2017 Glimp IP Ltd
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 # Run like:
-# $ blender.exe -b /path/to/glimpse-training.blend -P generator-cmd.py -- -h
+# $ blender.exe -b /path/to/glimpse-training.blend -P glimpse-cli.py -- --help-glimpse
 
 import os
 import sys
 import argparse
 import bpy
+import addon_utils
 
 if "--" in sys.argv:
     argv = sys.argv
@@ -14,7 +35,7 @@ else:
 
 parser = argparse.ArgumentParser(prog="glimpse-generator", add_help=False)
 
-parser.add_argument('--help-glimpse', help='Show this help message and exit', action='store_true')
+parser.add_argument('--help-glimpse', help='Show this help message and exit', action='help')
 parser.add_argument('--info', help='Load the mocap index and print summary information', action='store_true')
 parser.add_argument('--preload', help='Preload mocap files as actions before rendering', action='store_true')
 parser.add_argument('--purge', help='Purge mocap actions', action='store_true')
@@ -25,9 +46,28 @@ parser.add_argument('--name', default=os.getcwd(), help='Unique name for this re
 parser.add_argument('mocaps', help='Directory with motion capture files')
 args = parser.parse_args(argv)
 
-if args.help_glimpse:
-    parser.print_help();
-    bpy.ops.wm.quit_blender()
+addon_dependencies = [
+    'glimpse_data_generator',
+    'mesh_paint_rig',
+    'makeclothes',
+    'maketarget',
+    'makewalk',
+]
+
+dep_error = ""
+for dep in addon_dependencies:
+    addon_status = addon_utils.check(dep)
+    if addon_status[0] != True or addon_status[1] != True:
+        dep_error += "Addon '" + dep + "' has not been enabled through Blender's User Preferences\n"
+
+if dep_error != "":
+    print("\n")
+    print("Error:\n")
+    print(dep_error)
+    print("\n")
+    print("Please find the instructions for setting up the required addons in blender/README.md")
+    print("> https://github.com/glimpse-project/glimpse/blob/master/blender/README.md")
+    print("\n")
     sys.exit(1)
 
 if not os.path.isdir(args.mocaps):
