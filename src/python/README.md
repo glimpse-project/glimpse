b006e9feca2550299d97f22daf7a71e13f7310cf
Robert Bragg <robert@impossible.com>
2017-12-12 13:26:13 +0000

    Some tidying of the src/python/ module
    Sets the license to 'MIT', adds some missing copyright headers and
    avoids needing an intermediate libglimpse.so to be built which is
    awkward to locate now that we support out-of-tree builds. Since this was
    previously the only user of libglimpse.so we no longer build a glimpse
    shared library.  README.md updated accordingly.

--- src/python/README.md
+++ src/python/README.md
@@ -2,15 +2,15 @@
 
 ## Building
 
-It's recommended to run this inside a virtualenv, so as not to pollute the 
-system Python package index. First, ensure libglimpse.so is built in the 
-parent directory and that SWIG is installed and available in the PATH. Then, 
-run the following:
+It's recommended to run this inside a virtualenv, so as not to pollute the
+system Python package index.
+
+Build the glimpse module by running the following:
 
 ```
 make
-pip install dist/glimpse*.whl
+pip install --user --force-reinstall dist/glimpse*.whl
 ```
 
 The 'glimpse' module will now be available in Python. Make sure that 
-libglimpse.so is available in the library path before importing.
+libglimpse-python-tools.so is available in the library path before importing.
--- src/python/setup.py
+++ src/python/setup.py
@@ -1,4 +1,25 @@
 #! /usr/bin/env python
+#
+# Copyright (c) 2017 Glimp IP Ltd
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
 
 from setuptools import setup, Extension
 from distutils.command.build import build
@@ -17,9 +38,17 @@ class BuildExtFirst(build):
                     ('build_scripts', build.has_scripts)]
 
 glimpse = Extension('_glimpse',
-        ['glimpse.i', 'glimpse_python.cc'],
+        ['glimpse.i', 'glimpse_python.cc',
+         '../image_utils.cc',
+         '../infer.cc',
+         '../loader.cc',
+         '../tinyexr.cc',
+         '../parson.c',
+         '../llist.c',
+         '../xalloc.c'
+        ],
         include_dirs = [numpy_include],
-        libraries = ['glimpse'])
+        libraries = ['png'])
 
 setup(name = 'glimpse',
       description = 'A library for performing joint inference from depth images',
@@ -30,6 +59,6 @@ setup(name = 'glimpse',
       packages = [ 'glimpse' ],
       cmdclass = { 'build': BuildExtFirst },
       ext_modules = [ glimpse ],
-      license = 'Proprietary',
+      license = 'MIT',
       url = 'https://medium.com/impossible/glimpse-a-sneak-peak-into-your-creative-self-29bd2e656ff6',
       install_requires = [ 'numpy' ])
