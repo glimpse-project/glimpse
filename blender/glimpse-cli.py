a577a613efa66e98aa82d242265078c51051f0c4
Robert Bragg <robert@impossible.com>
2017-12-13 17:22:22 +0000

    blender/glimpse-cli: support running directly
    Instead of having to run the glimpse command line interface like:
    
      blender -b \
        glimpse-training.blend \
        -P glimpse-cli.py \
        -- \
        --help-glimpse
    
    glimpse-cli.py now recognises if it's called via blender or directly
    and if called directly then it will invoke blender as above
    automatically.

--- blender/glimpse-cli.py
+++ blender/glimpse-cli.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 # Copyright (c) 2017 Glimp IP Ltd
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -21,21 +23,32 @@
 # Run like:
 # $ blender.exe -b /path/to/glimpse-training.blend -P glimpse-cli.py -- --help-glimpse
 
+
+
 import os
 import sys
 import argparse
-import bpy
-import addon_utils
+import subprocess
 
-if "--" in sys.argv:
-    argv = sys.argv
-    argv = argv[argv.index("--") + 1:]
+try:
+    import bpy
+    import addon_utils
+    as_blender_addon = True
+except:
+    as_blender_addon = False
+
+if as_blender_addon:
+    add_help_glimpse_opt = False
 else:
-    argv = []
+    add_help_opt = True
 
-parser = argparse.ArgumentParser(prog="glimpse-generator", add_help=False)
+if as_blender_addon:
+    parser = argparse.ArgumentParser(prog="glimpse-generator", add_help=False)
+    parser.add_argument('--help-glimpse', help='Show this help message and exit', action='help')
+else:
+    parser = argparse.ArgumentParser(prog="glimpse-generator")
 
-parser.add_argument('--help-glimpse', help='Show this help message and exit', action='help')
+parser.add_argument('--debug', action='store_true', help=argparse.SUPPRESS)
 parser.add_argument('--info', help='Load the mocap index and print summary information', action='store_true')
 parser.add_argument('--preload', help='Preload mocap files as actions before rendering', action='store_true')
 parser.add_argument('--purge', help='Purge mocap actions', action='store_true')
@@ -43,8 +56,50 @@ parser.add_argument('--start', type=int, default=0, help='Index of first MoCap t
 parser.add_argument('--end', default=0, type=int, help='Index of last MoCap to render')
 parser.add_argument('--dest', default=os.getcwd(), help='Directory to write files too')
 parser.add_argument('--name', default=os.getcwd(), help='Unique name for this render run')
-parser.add_argument('mocaps', help='Directory with motion capture files')
-args = parser.parse_args(argv)
+parser.add_argument('training_data', help='Directory with all training data')
+
+
+
+#def run_cmd(args):
+#    if cli_args.debug:
+#        print("# " + " ".join(map(str, args)), file=sys.stderr)
+#    try:
+#        output = subprocess.check_output(args).decode("utf-8").strip()
+#    except:
+#        output = ""
+#    if cli_args.debug:
+#        print("# > " + "\n# > ".join(output.splitlines()))
+#
+#    return output
+
+
+def run_cmd(args):
+    if cli_args.debug:
+        print("# " + " ".join(map(str, args)), file=sys.stderr)
+        returncode = subprocess.call(args)
+        print("# return status = " + str(returncode))
+        return returncode
+    else:
+        return subprocess.call(args)
+
+if as_blender_addon:
+    if "--" in sys.argv:
+        argv = sys.argv
+        argv = argv[argv.index("--") + 1:]
+    else:
+        argv = []
+
+    cli_args = parser.parse_args(argv)
+else:
+    cli_args = parser.parse_args()
+    ret = run_cmd(['blender', '-b',
+                   os.path.join(cli_args.training_data, 'glimpse-training.blend'),
+                   '-P',
+                   sys.argv[0],
+                   '--'] +
+                   sys.argv[1:])
+    sys.exit(ret)
+
 
 addon_dependencies = [
     'glimpse_data_generator',
@@ -70,56 +125,57 @@ if dep_error != "":
     print("\n")
     sys.exit(1)
 
-if not os.path.isdir(args.mocaps):
-    print("Non-existent mocaps directory %s" % args.mocaps)
+mocaps_dir = os.path.join(cli_args.training_data, 'mocap')
+if not os.path.isdir(mocaps_dir):
+    print("Non-existent mocaps directory %s" % mocaps_dir)
     bpy.ops.wm.quit_blender()
     sys.exit(1)
-bpy.context.scene.GlimpseBvhRoot = args.mocaps
+bpy.context.scene.GlimpseBvhRoot = mocaps_dir
 
 bpy.ops.glimpse.open_bvh_index()
 
-if args.start:
-    bpy.context.scene.GlimpseBvhGenFrom = args.start
+if cli_args.start:
+    bpy.context.scene.GlimpseBvhGenFrom = cli_args.start
 
-if args.end:
-    bpy.context.scene.GlimpseBvhGenTo = args.end
+if cli_args.end:
+    bpy.context.scene.GlimpseBvhGenTo = cli_args.end
 
-if args.info:
+if cli_args.info:
     bpy.ops.glimpse.generator_info()
     bpy.ops.wm.quit_blender()
 
-if args.preload:
+if cli_args.preload:
     bpy.ops.glimpse.generator_preload()
     print("Saving to %s" % bpy.context.blend_data.filepath)
     bpy.ops.wm.save_as_mainfile(filepath=bpy.context.blend_data.filepath)
     bpy.ops.wm.quit_blender()
     
-if args.purge:
+if cli_args.purge:
     bpy.ops.glimpse.purge_mocap_actions()
     bpy.ops.wm.save_as_mainfile(filepath=bpy.context.blend_data.filepath)
     bpy.ops.wm.quit_blender()
 
-if args.dest == "":
+if cli_args.dest == "":
     print("--dest argument required in this case to find files to preload")
     bpy.ops.wm.quit_blender()
-if not os.path.isdir(args.dest):
-    print("Non-existent dest directory %s" % args.dest)
+if not os.path.isdir(cli_args.dest):
+    print("Non-existent dest directory %s" % cli_args.dest)
     bpy.ops.wm.quit_blender()
-bpy.context.scene.GlimpseDataRoot = args.dest
+bpy.context.scene.GlimpseDataRoot = cli_args.dest
 
-print("DataRoot: " + args.dest)
+print("DataRoot: " + cli_args.dest)
 
-if args.name == "":
+if cli_args.name == "":
     print("--name argument required in this case to find files to preload")
     bpy.ops.wm.quit_blender()
-bpy.context.scene.GlimpseGenDir = args.name
+bpy.context.scene.GlimpseGenDir = cli_args.name
 
 
 import cProfile
-cProfile.run("bpy.ops.glimpse.generate_data()", "glimpse-" + args.name + ".prof")
+cProfile.run("bpy.ops.glimpse.generate_data()", "glimpse-" + cli_args.name + ".prof")
  
 import pstats
-p = pstats.Stats("glimpse-" + args.name + ".prof")
+p = pstats.Stats("glimpse-" + cli_args.name + ".prof")
 p.sort_stats("cumulative").print_stats(20)
 
 
