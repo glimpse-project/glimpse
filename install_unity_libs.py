62e911f9d61e0562e25e0d5efae78b79ed199ef7
Robert Bragg <robert@impossible.com>
2018-01-18 15:35:41 +0000

    install_unity_libs: install libc++_shared.so on Android
    We are using a toolchain with STL = libc++_shared so we need to copy
    libc++_shared.so to the Plugins/ directory to packed by Unity into the
    installed .apk

--- install_unity_libs.py
+++ install_unity_libs.py
@@ -7,22 +7,38 @@ import subprocess
 import sys
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--subdir',
-                    help='The name of the Plugins/<arch> subdirectory')
+parser.add_argument('--plugins-subdir',
+                    help='The name of the subdirectory underneath Assets/Plugins/ to install libraries')
+parser.add_argument('--android-ndk-arch',
+                    help='The arch used to find a copy of libc++_shared.so in the Android NDK (e.g. armeabi-v7a, arm64-v8a, x86_64)')
 parser.add_argument('--strip',
-                    help='The tool for stripping share library symbols')
+                    help='The tool for stripping share library symbols',
+                    default='strip')
 args = parser.parse_args()
 
 introspect_cmd = os.environ['MESONINTROSPECT'].split()
 
 targets_json = subprocess.check_output(introspect_cmd + [ '--targets' ])
 
+if args.android_ndk_arch:
+    if 'ANDROID_NDK_HOME' not in os.environ:
+        print("")
+        print("ERROR: $ANDROID_NDK_HOME not set so install script can't find libc++_shared.so")
+        print("")
+        sys.exit(1)
+
+    ndk_path = os.environ['ANDROID_NDK_HOME']
+    libcxx_shared_path = os.path.join(ndk_path, 'sources/cxx-stl/llvm-libc++/libs',
+                                      args.android_ndk_arch, 'libc++_shared.so')
+
+    print("Installing libc++_shared.so from %s" % libcxx_shared_path)
+
 targets = json.loads(targets_json)
 
 if 'DESTDIR' in os.environ:
-    dst = os.path.join(os.environ['DESTDIR'], os.environ['MESON_INSTALL_PREFIX'], args.subdir)
+    dst = os.path.join(os.environ['DESTDIR'], os.environ['MESON_INSTALL_PREFIX'], args.plugins_subdir)
 else:
-    dst = os.path.join(os.environ['MESON_INSTALL_PREFIX'], args.subdir)
+    dst = os.path.join(os.environ['MESON_INSTALL_PREFIX'], args.plugins_subdir)
 
 print("DST = " + dst)
 
@@ -42,3 +58,10 @@ for target in targets:
                 os.path.join(dst, parts[0] + '.so')]
         print(" ".join(install_cmd))
         subprocess.check_call(install_cmd)
+
+if args.android_ndk_arch:
+    install_cmd = [ 'install', '-s' , '--strip-program', args.strip,
+                    libcxx_shared_path, dst]
+    print(" ".join(install_cmd))
+    subprocess.check_call(install_cmd)
+
