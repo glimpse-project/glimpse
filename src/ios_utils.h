c4d474535e33d382d3f185f66c98880c273836ad
Robert Bragg <robert@impossible.com>
2018-04-06 16:38:03 +0100

    ios: add utility support AVFoundation
    This adds a ios_utils api for using the AVFoundation camera api to
    capture video and depth data on iOS. This isn't integrated into
    glimpse_device yet though.

--- src/ios_utils.h
+++ src/ios_utils.h
@@ -8,6 +8,40 @@ extern "C" {
 char *
 ios_util_get_documents_path(void);
 
+char *
+ios_util_get_resources_path(void);
+
+void
+ios_log(const char *msg);
+
+struct ios_av_session;
+
+struct ios_av_session *
+ios_util_av_session_new(struct gm_logger *log,
+                        void (*configured_cb)(struct ios_av_session *session, void *user_data),
+                        void (*depth_cb)(struct ios_av_session *session,
+                                         struct gm_intrinsics *intrinsics,
+                                         int stride,
+                                         float *disparity,
+                                         void *user_data),
+                        void (*video_cb)(struct ios_av_session *session,
+                                         struct gm_intrinsics *intrinsics,
+                                         int stride,
+                                         uint8_t *video,
+                                         void *user_data),
+                        void *user_data);
+
+void
+ios_util_session_configure(struct ios_av_session *_session);
+
+void
+ios_util_session_start(struct ios_av_session *session);
+
+void
+ios_util_session_stop(struct ios_av_session *_session);
+
+void
+ios_util_session_destroy(struct ios_av_session *session);
 
 #ifdef __cplusplus
 }
