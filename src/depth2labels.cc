c7d17517b2e27de4ffa34abb797806fab75d946f
Robert Bragg <robert@impossible.com>
2018-06-13 12:09:36 +0100

    depth2labels: normalize background depth before inference
    The depth value used for the background in the test image may not match
    the background value that the decision tree expects so before running
    label inference the background is explicitly cleared to the bg_depth
    got from the decision tree.

--- src/depth2labels.cc
+++ src/depth2labels.cc
@@ -104,130 +104,137 @@ print_usage(FILE* stream)
 int
 main(int argc, char **argv)
 {
-  struct gm_logger *log = gm_logger_new(NULL, NULL);
-  bool write_palettized_pngs = true;
-  const char *palette_file = NULL;
-  int opt;
+    struct gm_logger *log = gm_logger_new(NULL, NULL);
+    bool write_palettized_pngs = true;
+    const char *palette_file = NULL;
+    int opt;
 
-  /* N.B. The initial '+' means that getopt will stop looking for options
-   * after the first non-option argument...
-   */
-  const char *short_options="+hgp:";
-  const struct option long_options[] = {
-      {"help",            no_argument,        0, 'h'},
-      {"grey",            no_argument,        0, 'g'},
-      {"palette",         required_argument,  0, 'p'},
-      {0, 0, 0, 0}
-  };
+    /* N.B. The initial '+' means that getopt will stop looking for options
+     * after the first non-option argument...
+     */
+    const char *short_options="+hgp:";
+    const struct option long_options[] = {
+        {"help",            no_argument,        0, 'h'},
+        {"grey",            no_argument,        0, 'g'},
+        {"palette",         required_argument,  0, 'p'},
+        {0, 0, 0, 0}
+    };
 
-  while ((opt = getopt_long(argc, argv, short_options, long_options, NULL))
-         != -1)
+    while ((opt = getopt_long(argc, argv, short_options, long_options, NULL))
+           != -1)
     {
-      switch (opt) {
-          case 'h':
-              print_usage(stderr);
-              return 0;
-          case 'g':
-              write_palettized_pngs = false;
-              break;
-          case 'p':
-              palette_file = optarg;
-              break;
-         default:
-              print_usage(stderr);
-              return 1;
-      }
-  }
-
-  if ((argc - optind) < 3)
-    {
-      print_usage(stderr);
-    }
-
-  // Read depth file
-  half* depth_image = NULL;
-  IUImageSpec exr_spec = { 0, 0, IU_FORMAT_HALF };
-  if (iu_read_exr_from_file(argv[optind], &exr_spec,
-                            (void**)(&depth_image)) != SUCCESS)
-    {
-      fprintf(stderr, "Error loading depth image\n");
-      return 1;
-    }
-  int width = exr_spec.width;
-  int height = exr_spec.height;
-
-  if (!write_palettized_pngs)
-    {
-      palette = NULL;
-    }
-  else if (palette_file)
-    {
-      palette = NULL;
-      if (iu_read_png_from_file(palette_file, NULL, NULL,
-                                (void**)(&palette), &palette_size) != SUCCESS)
-        {
-          fprintf(stderr, "Failed to read palette\n");
-          return 1;
+        switch (opt) {
+        case 'h':
+            print_usage(stderr);
+            return 0;
+        case 'g':
+            write_palettized_pngs = false;
+            break;
+        case 'p':
+            palette_file = optarg;
+            break;
+        default:
+            print_usage(stderr);
+            return 1;
         }
     }
 
-  // Do inference
-  int n_trees = argc - optind - 2;
-  RDTree* forest[n_trees];
+    if ((argc - optind) < 3)
+        print_usage(stderr);
 
-  for (int i = 0; i < n_trees; i++) {
-      char *tree_path = argv[optind + 2 + i];
-
-      JSON_Value *js = json_parse_file(tree_path);
-      gm_assert(log, js != NULL, "Failed to parse %s as JSON", tree_path);
-
-      forest[i] = rdt_tree_load_from_json(log, js, NULL); // abort on error
-
-      json_value_free(js);
-  }
-
-  float* output_pr = infer_labels<half>(forest, n_trees, depth_image,
-                                        width, height);
-  uint8_t n_labels = forest[0]->header.n_labels;
-
-  for (int i = 0; i < n_trees; i++) {
-      rdt_tree_destroy(forest[i]);
-  }
-
-  // Write out png of most likely labels
-  png_bytep out_labels = (png_bytep)xmalloc(width * height);
-  for (int y = 0; y < height; y++)
+    // Read depth file
+    half* depth_image = NULL;
+    IUImageSpec exr_spec = { 0, 0, IU_FORMAT_HALF };
+    if (iu_read_exr_from_file(argv[optind], &exr_spec,
+                              (void**)(&depth_image)) != SUCCESS)
     {
-      for (int x = 0; x < width; x++)
+        fprintf(stderr, "Error loading depth image\n");
+        return 1;
+    }
+    int width = exr_spec.width;
+    int height = exr_spec.height;
+
+    if (!write_palettized_pngs) {
+        palette = NULL;
+    } else if (palette_file) {
+        palette = NULL;
+        if (iu_read_png_from_file(palette_file, NULL, NULL,
+                                  (void**)(&palette), &palette_size) != SUCCESS)
         {
-          uint8_t label = 0;
-          float pr = 0.0;
-          float* pr_table = &output_pr[(y * width * n_labels) +
-                                       (x * n_labels)];
-          for (uint8_t l = 0; l < n_labels; l++)
-            {
-              if (pr_table[l] > pr)
-                {
-                  label = l;
-                  pr = pr_table[l];
+            fprintf(stderr, "Failed to read palette\n");
+            return 1;
+        }
+    }
+
+    // Do inference
+    int n_trees = argc - optind - 2;
+    RDTree* forest[n_trees];
+
+    for (int i = 0; i < n_trees; i++) {
+        char *tree_path = argv[optind + 2 + i];
+
+        JSON_Value *js = json_parse_file(tree_path);
+        gm_assert(log, js != NULL, "Failed to parse %s as JSON", tree_path);
+
+        forest[i] = rdt_tree_load_from_json(log, js, NULL); // abort on error
+
+        json_value_free(js);
+    }
+
+    uint8_t n_labels = forest[0]->header.n_labels;
+    float bg_depth = forest[0]->header.bg_depth;
+
+    /* Although the training images may use a background depth of 1000.0f they
+     * might also contain NAN values and the decision tree might have been
+     * trained to expect a different depth for the background pixels...
+     */
+    for (int y = 0; y < height; y++) {
+        for (int x = 0; x < width; x++) {
+            int off = width * y + x;
+
+            float depth_m = depth_image[off];
+            if (depth_m >= HUGE_DEPTH || std::isnan(depth_m)) {
+                depth_image[off] = bg_depth;
+            }
+        }
+    }
+
+    float* output_pr = infer_labels<half>(forest, n_trees, depth_image,
+                                          width, height);
+
+    // Write out png of most likely labels
+    png_bytep out_labels = (png_bytep)xcalloc(1, width * height);
+    for (int y = 0; y < height; y++) {
+        for (int x = 0; x < width; x++) {
+            float* pr_table = &output_pr[(y * width * n_labels) +
+                (x * n_labels)];
+            uint8_t label = 0;
+            float pr = pr_table[0];
+            for (uint8_t l = 1; l < n_labels; l++) {
+                if (pr_table[l] > pr) {
+                    label = l;
+                    pr = pr_table[l];
                 }
             }
 
-          out_labels[y * width + x] = label;
+            out_labels[y * width + x] = label;
         }
     }
 
-  IUImageSpec output_spec = { width, height, IU_FORMAT_U8 };
-  if (iu_write_png_to_file(argv[optind+1], &output_spec,
-                           out_labels, palette, palette_size) != SUCCESS)
+    IUImageSpec output_spec = { width, height, IU_FORMAT_U8 };
+    if (iu_write_png_to_file(argv[optind+1], &output_spec,
+                             out_labels, palette, palette_size) != SUCCESS)
     {
-      fprintf(stderr, "Error writing output PNG\n");
+        fprintf(stderr, "Error writing output PNG\n");
     }
 
-  // Clean-up
-  xfree(out_labels);
-  xfree(output_pr);
-  xfree(depth_image);
+    // Clean-up
+    for (int i = 0; i < n_trees; i++) {
+        rdt_tree_destroy(forest[i]);
+    }
+    xfree(out_labels);
+    xfree(output_pr);
+    xfree(depth_image);
 
-  return 0;
+    return 0;
 }
