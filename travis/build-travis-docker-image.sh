e35a1d84a26c132c3a03b26bea8ce82b939eedac
Robert Bragg <robert@impossible.com>
2019-01-11 15:45:21 +0000

    Update travis docker image including windows-sdk

--- travis/build-travis-docker-image.sh
+++ travis/build-travis-docker-image.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-sudo docker build -t glimpseproject/glimpse-travis .
+if ! test -f windows-sdk-10.0.17134.0.tar.bz2; then
+    echo "windows-sdk-10.0.17134.0.tar.bz2 should be copied to travis/ directory first"
+    exit 1
+fi
+sudo docker build -t rib1/glimpse-travis .
