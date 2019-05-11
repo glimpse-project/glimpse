78acdfc19ca49eed79116273959e6bf4451d872a
Robert Bragg <robert@impossible.com>
2019-05-11 20:13:41 +0100

    build-travis-docker-image: fix absolute paths in windows-sdk

--- travis/build-travis-docker-image.sh
+++ travis/build-travis-docker-image.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
+if ! test -d windows-sdk; then
+    echo "Missing required windows-sdk directory (build with ../windows-sdk-build.py"
+fi
+
+# Fix up absolute paths in the Windows SDK:
+sed -i -r "s|sdk_path = .+|sdk_path = '/windows-sdk'|g" windows-sdk/meson/windows-x64-debug-cross-file.txt
+sed -i -r "s|sdk_path = .+|sdk_path = '/windows-sdk'|g" windows-sdk/meson/windows-x64-release-cross-file.txt
+
 sudo docker build -t rib1/glimpse-travis .
